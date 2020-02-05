95bae86ad21ec806b5029895cec49307a07eead1
Ezio Santori <Ezio.Santori@wolterskluwer.com>
2020-02-05 07:32:20 +0100

    Completed 04 section Added category operations

--- Uplift/Areas/Admin/Controllers/CategoryController.cs
+++ Uplift/Areas/Admin/Controllers/CategoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.Models;
 
 namespace Uplift.Areas.Admin.Controllers
 {
@@ -19,5 +20,68 @@ namespace Uplift.Areas.Admin.Controllers
     {
       return View();
     }
+    public IActionResult Upsert(int? id)
+    {
+      var item = new Category();
+      if (id == null) {
+        return View(item);
+      }
+      item = _unitOfWork.Category.Get(id.Value);
+      if (item == null)
+      {
+        return NotFound();
+      }
+
+      return View(item);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Upsert(Category category)
+    {
+      if (ModelState.IsValid)
+      {
+        if (category.Id == 0)
+        {
+          _unitOfWork.Category.Add(category);
+        }
+        else
+        {
+          _unitOfWork.Category.Update(category);
+        }
+        _unitOfWork.Save();
+        return RedirectToAction(nameof(Index));
+      }
+      return View(category);
+    }
+
+    #region API CALLS
+    [HttpGet]
+    public IActionResult GetAll() {
+      return Json(new {
+        data = _unitOfWork.Category.GetAll()
+      });
+    }
+    [HttpDelete]
+    public IActionResult Delete(int id)
+    {
+      var obj = _unitOfWork.Category.Get(id);
+      if (obj == null)
+      {
+        return Json(new { 
+          success=false,
+          message="Error while deleting"
+        });
+      }
+
+      _unitOfWork.Category.Remove(obj);
+      _unitOfWork.Save();
+      return Json(new
+      {
+        success = true,
+        message = "Delete successfully"
+      });
+    }
+    #endregion
   }
 }
